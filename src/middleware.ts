d2e4d4765deabc66c731f1edfbe1ca32d23e4889
apucsd <apusutradhar77@gmail.com>
2024-11-16 18:11:28 +0600

    added middleware for private route

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+      // console.log('Middleware running');
+
+      const token = cookies().get('accessToken')?.value;
+      // console.log('Token:', token);
+
+      if (!token) {
+            console.log('No token found, redirecting...');
+            return NextResponse.redirect(new URL('/', request.url));
+      }
+
+      return NextResponse.next();
+}
+
+export const config = {
+      matcher: ['/profile/:path*', '/pricing/:path*'],
+};
